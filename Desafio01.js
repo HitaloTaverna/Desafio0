727683b9994eba81eb9604365d65e5219a923dc7
Hítalo <hitalo.taverna.quintal@gmail.com>
2022-06-28 16:26:23 -0300

    finalização do desafio01

--- Desafio01.js
+++ Desafio01.js
@@ -8,37 +8,42 @@ export const Desafio01 =()=> {
               <View style={{width:'100%', height: 25, backgroundColor: "#50E3C2" }}/>
           </View>
                   {/* 1° Quadrado */}
-          <View style= {{flex: 1.4, alignItems: 'center'}}>
+          <View style= {{flex: 1.4, alignItems: 'center', justifyContent: 'flex-end'}}>
             <View style= {{ width:100,height: 100, backgroundColor:"#F5A623"}}/> 
          </View>
 
-          <View style={{flex:3}}>
+               {/* 1° retângulo */}
+      <View style= {{flex: 0.60, alignItems: 'center', justifyContent: 'center' }}>
+            <View style= {{width:140,height: 30, backgroundColor:"#F5A623" }}/> 
+      </View>
+
+                           {/* 2°, 3°, 4° retângulos */}
+      <View style= {{flex:1}}>    
+      <View style= {{flex: 0.01, justifyContent:'space-between', alignItems: 'flex-end'}}>
+            <View style= {{ width: '50%', height: 50, backgroundColor:"#4A90E2" }}/> 
+      </View>
+            <View style= {{width: '50%', height: 50, backgroundColor:"#9013FE"}}/>  
+            <View style= {{flexDirection: 'row-reverse'}}>
+      </View>
+            <View style= {{ height: 13, backgroundColor:"#50E3C2"}}/> 
+      </View>
+
                   {/* Todos os quadrados */}
-              <View style = {{flex:1, justifyContent: "space-around", alignItems: "center", flexDirection: "row"}}/>
-                <View style= {{width: 100, height: 100, backgroundColor: "#F5A623"}}/>
-                <View style= {{width: 100, height: 100, backgroundColor: "#F5A623"}}/>
-                <View style= {{width: 100, height: 100, backgroundColor: "#F5A623"}}/>
+      <View style={{flex:6}}>
           <View style = {{flex:1, justifyContent: "space-around", alignItems: "center", flexDirection: "row"}}>
               <View style= {{width: 100, height: 100, backgroundColor: "#F5A623"}}/>
               <View style= {{width: 100, height: 100, backgroundColor: "#F5A623"}}/>
               <View style= {{width: 100, height: 100, backgroundColor: "#F5A623"}}/>
-          </View>
-                   {/* 1° retângulo */}
-      <View style= {{flex: 0.7, alignItems: 'center'}}>
-            <View style= {{width:140,height: 30, backgroundColor:"#F5A623" }}/> 
-         </View>
-                   {/* 2° retângulo */}
-         <View style= {{flex:1}}>
-            <View style= {{flexDirection: "row"}}>
-               <View style= {{flex: 1, height: 78, backgroundColor:"#9013FE"}}/> 
-               <View style= {{flex: 1, height: 78, backgroundColor:"#9013FE"}}/> 
-            </View>
-            <View style= {{height: 13, backgroundColor:"#50E3C2"}}/> 
-         </View>
-                 {/* 3° retângulo */}
-         <View style= {{flex: 0.75, justifyContent:'flex-end'}}>
-            <View style= {{width: '100%', height: 60, backgroundColor:"#4A90E2" }}/> 
-         </View>
+      </View>
+          <View style = {{flex:1, justifyContent: "space-around", alignItems: "center", flexDirection: "row"}}>
+              <View style= {{width: 100, height: 100, backgroundColor: "#F5A623"}}/>
+              <View style= {{width: 100, height: 100, backgroundColor: "#F5A623"}}/>
+              <View style= {{width: 100, height: 100, backgroundColor: "#F5A623"}}/>
+      </View>
+                  {/* retângulo inferior*/}
+      <View style= {{flex: 0, justifyContent:'space-between'}}>
+            <View style= {{width: '100%', height: 50, backgroundColor:"#4A90E2" }}/> 
+      </View>
 
       </View>
           
